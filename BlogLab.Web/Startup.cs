c53f7106381d8f0ec35f0799fb075b47ae2ce805
Trung Kien Huynh <trungkien.huynh@gmail.com>
2021-12-01 15:21:33 +0100

    Added AccountControlller

--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -1,14 +1,24 @@
+using BlogLab.Identity;
+using BlogLab.Models.Account;
+using BlogLab.Repository;
+using BlogLab.Services;
+using BlogLab.Web.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogLab.Web
@@ -17,6 +27,7 @@ namespace BlogLab.Web
     {
         public Startup(IConfiguration configuration)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); 
             Configuration = configuration;
         }
 
@@ -25,8 +36,50 @@ namespace BlogLab.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddScoped<ITokenService, TokenService>()
+                .AddScoped<IBlogRepository, BlogRepository>()
+                .AddScoped<IBlogCommentRepository, BlogCommentRepository>()
+                .AddScoped<IAccountRepository, AccountRepository>()
+                .AddScoped<IPhotoRepository, PhotoRepository>();
+
+            services.AddIdentityCore<ApplicationUserIdentity>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddUserStore<UserStore>()
+                .AddDefaultTokenProviders()
+                .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
 
             services.AddControllers();
+            services.AddCors();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer
+                (
+                    options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = Configuration["Jwt:Issuer"],
+                            ValidAudience = Configuration["Jwt:Issuer"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                            ClockSkew = TimeSpan.Zero
+                        };
+                    }
+                );
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BlogLab.Web", Version = "v1" });
@@ -43,8 +96,20 @@ namespace BlogLab.Web
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BlogLab.Web v1"));
             }
 
+            app.ConfigureExceptionHandler();
+
             app.UseRouting();
 
+            if (env.IsDevelopment())
+            {
+                app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            } 
+            else
+            {
+                app.UseCors(options => options.WithOrigins("https://myWebsite.com"));
+            }
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
